8fd15b0b6cd4ff20e030383cb55d69dd87b2b6b8
sthathatha <lhdsecon@gmail.com>
2024-12-25 19:02:14 +0900

    フォルダ構成変更

---
+++ Assets/GameSource/InGame/script/GameMainSystem.cs
@@ -0,0 +1,180 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+/// <summary>
+/// ゲーム管理
+/// </summary>
+public class GameMainSystem : MainScriptBase
+{
+    /// <summary>インスタンス</summary>
+    public static GameMainSystem Instance { get; private set; }
+
+    #region 定数
+
+    #endregion
+
+    #region 変数・メンバー
+
+    /// <summary>FPS表示</summary>
+    public TMP_Text txt_fps;
+
+    private Vector2Int player_loc;
+    /// <summary>プレイヤー</summary>
+    public PlayerScript playerScript { get; set; }
+
+    /// <summary>ゲーム状態</summary>
+    public enum GameState
+    {
+        Loading = 0,
+        Active,
+        Exiting,
+    }
+    public GameState state { get; private set; } = GameState.Loading;
+
+    #endregion
+
+    #region 基底
+
+    /// <summary>コンストラクタでインスタンス設定</summary>
+    public GameMainSystem() { Instance = this; }
+
+    /// <summary>消滅時にインスタンスnull</summary>
+    private void OnDestroy() { Instance = null; }
+
+    /// <summary>
+    /// 開始前
+    /// </summary>
+    /// <returns></returns>
+    public override IEnumerator BeforeInitFadeIn()
+    {
+        yield return base.BeforeInitFadeIn();
+        var manager = ManagerSceneScript.GetInstance();
+
+        // 初期配置フィールド読み込み
+        RefreshFieldCell(true);
+        // 読み込み待ち
+        yield return new WaitWhile(() => manager.IsLoadingSubScene());
+
+        //todo:キャラクター読み込み
+        manager.LoadSubScene("GameSceneDrows", 0, 0);
+        // 読み込み待ち
+        yield return new WaitWhile(() => manager.IsLoadingSubScene());
+    }
+
+    /// <summary>
+    /// フェードイン後
+    /// </summary>
+    /// <param name="init"></param>
+    /// <returns></returns>
+    public override IEnumerator AfterFadeIn(bool init)
+    {
+        yield return base.AfterFadeIn(init);
+        state = GameState.Active;
+
+        StartCoroutine(UpdateCoroutine());
+
+        //todo:x秒毎にFPS表示
+        StartCoroutine(Test_DisplayFPS());
+    }
+
+    private IEnumerator Test_DisplayFPS()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            var fps = Mathf.Round(100.0f / Time.deltaTime) / 100f;
+            txt_fps.SetText(fps.ToString());
+        }
+    }
+
+    #endregion
+
+    #region メイン処理
+    /// <summary>
+    /// メイン処理
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator UpdateCoroutine()
+    {
+        while (true)
+        {
+            yield return null;
+
+            RefreshFieldCell();
+        }
+
+        //state = GameState.Exiting;
+        //todo:ゲーム終了
+    }
+    #endregion
+
+    #region フィールドマス管理
+
+    /// <summary>
+    /// フィールド生成
+    /// </summary>
+    /// <param name="init">初期化時</param>
+    private void RefreshFieldCell(bool init = false)
+    {
+        var manager = ManagerSceneScript.GetInstance();
+
+        if (init || (playerScript == null))
+        {
+            player_loc = Vector2Int.zero;
+        }
+        else
+        {
+            // プレイヤーの現在位置を取得
+            var nowPos = playerScript.transform.position;
+            var nowLoc = FieldUtil.GetFieldLoc(nowPos.x, nowPos.z);
+            if (player_loc == nowLoc) return; // 更新の必要なし
+            player_loc = nowLoc;
+        }
+
+        // 現在の位置から距離３以上のフィールドを破棄
+        var subList = manager.GetSubSceneList();
+        var releaseList = new List<GameFieldSystem>();
+        foreach (var sub in subList)
+        {
+            if (sub is not GameFieldSystem) continue;
+            var fld = sub as GameFieldSystem;
+            var dist = FieldUtil.CalcLocationDistance(fld.fieldCell, player_loc);
+            if (dist <= 2) continue;
+
+            releaseList.Add(fld);
+        }
+        foreach (var fld in releaseList)
+        {
+            fld.ReleaseField();
+        }
+
+        // ローディング中のもの
+        var loadingSubList = manager.GetSubSceneLoadingList();
+
+        // 現在の位置から距離１までのフィールドを読み込み
+        var createLocList = FieldUtil.GetAroundLocations(player_loc);
+        //var createLocList = new List<Vector2Int>();
+        createLocList.Add(player_loc);
+        foreach (var loc in createLocList)
+        {
+            // すでにあったら無視
+            if (subList.Any(sub =>
+                sub is GameFieldSystem && (sub as GameFieldSystem).fieldCell == loc)
+            )
+                continue;
+            if (loadingSubList.Any(sub => sub.prmList.Count == 2 &&
+                sub.prmList[0] == loc.x && sub.prmList[1] == loc.y))
+                continue;
+
+            // 読み込み
+            manager.LoadSubScene("GameSceneField1", loc.x, loc.y);
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/GameSource/InGame/script/character/PlayerScript.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// プレイヤー
+/// </summary>
+public class PlayerScript : CharacterScript
+{
+    #region メンバー
+
+    /// <summary>移動最高速</summary>
+    public float run_speed = 10f;
+
+    #endregion
+
+    #region 基底
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    protected override IEnumerator InitCharacter()
+    {
+        var cam = ManagerSceneScript.GetInstance().GetCamera3D();
+        cam.SetCameraDist(10f);
+        CameraControl(true);
+
+        // ゲームメインに自分を渡す
+        GameMainSystem.Instance.playerScript = this;
+        yield break;
+    }
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    protected override void UpdateCharacter()
+    {
+        if (GameMainSystem.Instance.state != GameMainSystem.GameState.Active) return;
+
+        MoveControl();
+        UpdateAnim();
+    }
+
+    /// <summary>
+    /// 後で走る更新
+    /// </summary>
+    protected override void UpdateCharacter2()
+    {
+        if (GameMainSystem.Instance.state != GameMainSystem.GameState.Active) return;
+        CameraControl();
+    }
+
+    #endregion
+
+    #region 移動操作
+
+    /// <summary>
+    /// 移動操作
+    /// </summary>
+    private void MoveControl()
+    {
+        // Y速度は保持
+        var vy = rigid.velocity.y;
+
+        var keyboard = Keyboard.current;
+        var gamepad = Gamepad.current;
+        // 移動ベース
+        //var stick = gamepad.leftStick.ReadValue();
+        var stick = new Vector3();
+        if (keyboard.dKey.isPressed) stick.x = 1f;
+        else if (keyboard.aKey.isPressed) stick.x = -1f;
+        if (keyboard.wKey.isPressed) stick.y = 1f;
+        else if (keyboard.sKey.isPressed) stick.y = -1f;
+        if (stick.magnitude < 0.1f)
+        {
+            rigid.velocity = new Vector3(0, vy, 0);
+            return;
+        }
+
+        // カメラ回転のy成分
+        var cam = ManagerSceneScript.GetInstance().GetCamera3D();
+        var camRot = cam.RotateLR * Mathf.Rad2Deg;
+
+        // スティック値をカメラ回転と合成
+        var move = new Vector3(stick.x, 0, stick.y);
+        move = Quaternion.Euler(0, camRot, 0) * move * run_speed;
+        rigid.velocity = new Vector3(move.x, vy, move.z);
+
+        // キャラの向き
+        var chrRot = Quaternion.LookRotation(move, new Vector3(0, 1f, 0));
+        transform.rotation = chrRot;
+    }
+
+    #endregion
+
+    #region カメラ操作
+
+    /// <summary>
+    /// カメラ操作
+    /// </summary>
+    /// <param name="noInput">入力不可</param>
+    private void CameraControl(bool noInput = false)
+    {
+        var keyboard = Keyboard.current;
+        var gamepad = Gamepad.current;
+        var stick = Vector3.zero;
+        if (!noInput)
+        {
+            // stick = gamepad.rightStick.ReadValue();
+            if (keyboard.rightArrowKey.isPressed) stick.x = 1f;
+            else if (keyboard.leftArrowKey.isPressed) stick.x = -1f;
+            if (keyboard.upArrowKey.isPressed) stick.y = 1f;
+            else if (keyboard.downArrowKey.isPressed) stick.y = -1f;
+        }
+
+        var cam = ManagerSceneScript.GetInstance().GetCamera3D();
+
+        cam.SetTargetPos(transform.position + new Vector3(0, 0.6f, 0));
+        if (Mathf.Abs(stick.x) > 0.1f)
+            cam.SetRotateLR(stick.x);
+        if (Mathf.Abs(stick.y) > 0.1f)
+            cam.SetRotateUD(stick.y);
+
+    }
+
+    #endregion
+
+    #region モデルアニメーション制御
+
+    /// <summary>
+    /// モデルアニメーション制御
+    /// </summary>
+    private void UpdateAnim()
+    {
+        var v = rigid.velocity;
+        v.y = 0f;
+        var spd = v.magnitude / 4f;
+        anim?.SetFloat("Speed", spd);
+        anim?.SetFloat("MotionSpeed", spd);
+    }
+
+    #endregion
+}
